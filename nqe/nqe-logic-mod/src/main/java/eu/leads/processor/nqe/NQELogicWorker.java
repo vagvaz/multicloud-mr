a847702b070c2af290e134337ac11154837da3e7
Evangelos Vazaios <vagvaz@gmail.com>
2015-06-23 10:26:35 +0100

    ADD the handlers for remote call of remote execute and completed

--- nqe/nqe-logic-mod/src/main/java/eu/leads/processor/nqe/NQELogicWorker.java
+++ nqe/nqe-logic-mod/src/main/java/eu/leads/processor/nqe/NQELogicWorker.java
@@ -1,5 +1,6 @@
 package eu.leads.processor.nqe;
 
+import eu.leads.processor.common.StringConstants;
 import eu.leads.processor.conf.LQPConfiguration;
 import eu.leads.processor.core.Action;
 import eu.leads.processor.core.ActionStatus;
@@ -9,6 +10,7 @@ import eu.leads.processor.core.net.DefaultNode;
 import eu.leads.processor.core.net.MessageUtils;
 import eu.leads.processor.core.net.Node;
 
+import eu.leads.processor.imanager.IManagerConstants;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
@@ -22,136 +24,180 @@ import static eu.leads.processor.core.ActionStatus.PENDING;
  */
 public class NQELogicWorker extends Verticle implements LeadsMessageHandler {
 
-  private final String componentType = "nqe";
-  JsonObject config;
-  String monitor;
-  String nqeGroup;
-  LogProxy log;
-  Node com;
-  String id;
-  String workQueueAddress;
-  String currentCluster;
+    private final String componentType = "nqe";
+    JsonObject config;
+    String monitor;
+    String nqeGroup;
+    LogProxy log;
+    Node com;
+    String id;
+    String workQueueAddress;
+    String currentCluster;
 
-  @Override
-  public void start() {
-    super.start();
-    LQPConfiguration.initialize();
-    config = container.config();
-    monitor = config.getString("monitor");
-    nqeGroup = config.getString("nqe");
-    workQueueAddress = config.getString("workqueue");
-    id = config.getString("id");
-    com = new DefaultNode();
-    com.initialize(id, nqeGroup, null, this, null, vertx);
-    log = new LogProxy(config.getString("log"), com);
-  }
-
-  @Override
-  public void stop() {
-    super.stop();
-    com.unsubscribeFromAll();
-  }
-
-  @Override
-  public void handle(JsonObject msg) {
-    String type = msg.getString("type");
-    String from = msg.getString(MessageUtils.FROM);
-    String to = msg.getString(MessageUtils.TO);
-
-    log.info("in NQELogicWorker.handle");
-
-    if (type.equals("action")) {
-      Action action = new Action(msg);
-      String label = action.getLabel();
-      Action newAction = null;
-      action.setProcessedBy(id);
-//         action.setStatus(ActionStatus.INPROCESS.toString());
-
-      switch (ActionStatus.valueOf(action.getStatus())) {
-        case PENDING: //probably received an action from an external source
-          if (label.equals(NQEConstants.DEPLOY_OPERATOR)) {
-            action.getData().putString("replyTo", action.getData().getString("monitor"));
-            action.setStatus(ActionStatus.INPROCESS.toString());
-            com.sendWithEventBus(workQueueAddress, action.asJsonObject());
-          } else if ((label.equals(NQEConstants.DEPLOY_PLUGIN))
-                     || (label.equals(NQEConstants.UNDEPLOY_PLUGIN))) {
-            action.setStatus(ActionStatus.INPROCESS.toString());
-            com.sendWithEventBus(workQueueAddress, action.asJsonObject());
-          } else if (label.equals(NQEConstants.DEPLOY_REMOTE_OPERATOR)) {
-            System.err.println("RECEVEIVED REMOTE DEPLOY!!!!!!!!!!");
-            action.setStatus(ActionStatus.INPROCESS.toString());
-            com.sendWithEventBus(workQueueAddress, action.asJsonObject());
-          } else if (label.equals(NQEConstants.EXECUTE_MAP_REDUCE_JOB)) {
-            String actionId = UUID.randomUUID().toString();
-            action.setId(actionId);
-
-            // TODO(ap0n): save id to the job caches we must add it here
-
-            com.sendTo(from, new JsonObject().putString("message", actionId));
-            com.sendWithEventBus(workQueueAddress, action.asJsonObject());
-          } else {
-            log.error("Unknown PENDING Action received " + action.toString());
-            return;
-          }
-          action.setStatus(ActionStatus.INPROCESS.toString());
-          if (newAction != null) {
-            action.addChildAction(newAction.getId());
-            logAction(newAction);
-          }
-          logAction(action);
-          break;
-        case INPROCESS: //  probably received an action from internal source (processors)
-        case COMPLETED: // the action either a part of a multistep workflow (INPROCESSING) or it could be processed.
-          if (label.equals(NQEConstants.DEPLOY_OPERATOR)) {
-            com.sendTo(action.getData().getString("replyTo"), action.getResult());
-          } else if ((label.equals(NQEConstants.DEPLOY_PLUGIN)) || (label.equals(
-              NQEConstants.UNDEPLOY_PLUGIN))) {
-//                      action.setStatus(ActionStatus.INPROCESS.toString());
-//                      com.sendWithEventBus((workQueueAddress,action.asJsonObject());
-          } else if (label.equals(NQEConstants.DEPLOY_REMOTE_OPERATOR)) {
-            newAction = new Action(action);
-            newAction.setData(action.getData());
-            newAction.getData().putString("microcloud", currentCluster);
-            newAction.getData().putString("STATUS", "SUCCESS");
-          } else {
-            log.error("Unknown COMPLETED OR INPROCESS Action received " + action.toString());
-            return;
-          }
-          finalizeAction(action);
-      }
+    @Override public void start() {
+        super.start();
+        LQPConfiguration.initialize();
+        config = container.config();
+        monitor = config.getString("monitor");
+        nqeGroup = config.getString("nqe");
+        workQueueAddress = config.getString("workqueue");
+        id = config.getString("id");
+        com = new DefaultNode();
+        com.initialize(id, nqeGroup, null, this, null, vertx);
+        log = new LogProxy(config.getString("log"), com);
     }
-  }
 
-  private void finalizeAction(Action action) {
-    //TODO
-    //1 inform monitor about completion (if it is completed in this logic each action requires 1 step processing
-    //2 remove from processing if necessary
-    //3 update action to persistence service
-  }
+    @Override public void stop() {
+        super.stop();
+        com.unsubscribeFromAll();
+    }
+
+    @Override public void handle(JsonObject msg) {
+        String type = msg.getString("type");
+        String from = msg.getString(MessageUtils.FROM);
+        String to = msg.getString(MessageUtils.TO);
+
+        log.info("in NQELogicWorker.handle");
+
+        if (type.equals("action")) {
+            Action action = new Action(msg);
+            String label = action.getLabel();
+            Action newAction = null;
+            action.setProcessedBy(id);
+            //         action.setStatus(ActionStatus.INPROCESS.toString());
+
+            switch (ActionStatus.valueOf(action.getStatus())) {
+                case PENDING: //probably received an action from an external source
+                    if (label.equals(NQEConstants.DEPLOY_OPERATOR)) {
+                        action.getData()
+                            .putString("replyTo", action.getData().getString("monitor"));
+                        action.setStatus(ActionStatus.INPROCESS.toString());
+                        com.sendWithEventBus(workQueueAddress, action.asJsonObject());
+                    } else if ((label.equals(NQEConstants.DEPLOY_PLUGIN)) || (label
+                        .equals(NQEConstants.UNDEPLOY_PLUGIN))) {
+                        action.setStatus(ActionStatus.INPROCESS.toString());
+                        com.sendWithEventBus(workQueueAddress, action.asJsonObject());
+                    } else if (label.equals(NQEConstants.DEPLOY_REMOTE_OPERATOR)) {
+                        System.err.println("RECEVEIVED REMOTE DEPLOY!!!!!!!!!!");
+                        action.setStatus(ActionStatus.INPROCESS.toString());
+                        com.sendWithEventBus(workQueueAddress, action.asJsonObject());
+                    } else if (label.equals(NQEConstants.EXECUTE_MAP_REDUCE_JOB)) {
+                        String actionId = UUID.randomUUID().toString();
+                        action.setId(actionId);
+
+                        // TODO(ap0n): save id to the job caches we must add it here
+                        // not here but yes this should be done
+                        //this is not correct
+                        com.sendTo(from, new JsonObject().putString("message", actionId));
+                        com.sendWithEventBus(workQueueAddress, action.asJsonObject());
+                    } else if (label.equals(IManagerConstants.EXECUTE_MAPREDUCE)) {
+                        //handle the complted mapreduce
+                        JsonObject webServiceReply = action.getResult().getObject("status");
+                        com.sendTo(action.getData().getString("replyTo"),webServiceReply);
+                        newAction = createNewAction(action);
+                        newAction.setLabel(NQEConstants.DEPLOY_REMOTE_OPERATOR);
+                        newAction.setDestination((StringConstants.NODEEXECUTORQUEUE));
+                        newAction.setData(action.getResult().getObject("result"));
+                        com.sendTo(newAction.getDestination(),newAction.asJsonObject());
+
+                    }
+                    else if (label.equals(IManagerConstants.COMPLETED_MAPREDUCE)){
+                        JsonObject webServiceReply = action.getResult().getObject("status");
+                        com.sendTo(action.getData().getString("replyTo"),webServiceReply);
+                        newAction = createNewAction(action);
+                        newAction.setLabel(NQEConstants.OPERATOR_COMPLETE);
+                        newAction.setDestination(action.getResult().getString("replyGroup"));
+                        newAction.setData(action.getResult().getObject("result"));
+                        newAction.setStatus(ActionStatus.COMPLETED.toString());
+                        com.sendTo(newAction.getDestination(),newAction.asJsonObject());
+                    }
+                    else {
+                        log.error("Unknown PENDING Action received " + action.toString());
+                        return;
+                    }
+                    action.setStatus(ActionStatus.INPROCESS.toString());
+                    if (newAction != null) {
+                        action.addChildAction(newAction.getId());
+                        logAction(newAction);
+                    }
+                    logAction(action);
+                    break;
+                case INPROCESS: //  probably received an action from internal source (processors)
+                case COMPLETED: // the action either a part of a multistep workflow (INPROCESSING) or it could be processed.
+                    if (label.equals(NQEConstants.DEPLOY_OPERATOR)) {
+                        com.sendTo(action.getData().getString("replyTo"), action.getResult());
+                    } else if ((label.equals(NQEConstants.DEPLOY_PLUGIN)) || (label
+                        .equals(NQEConstants.UNDEPLOY_PLUGIN))) {
+                        //                      action.setStatus(ActionStatus.INPROCESS.toString());
+                        //                      com.sendWithEventBus((workQueueAddress,action.asJsonObject());
+                    } else if (label.equals(NQEConstants.DEPLOY_REMOTE_OPERATOR)) {
+                        newAction = new Action(action);
+                        newAction.setData(action.getData());
+                        newAction.getData().putString("microcloud", currentCluster);
+                        newAction.getData().putString("STATUS", "SUCCESS");
+                    } else if (label.equals(NQEConstants.EXECUTE_MAP_REDUCE_JOB)){
+                        //here reply to the webservice with the ID of the job
+                    } else if (label.equals(IManagerConstants.EXECUTE_MAPREDUCE)){
+                        //send the completed message to the correct nqe
+                        JsonObject webServiceReply = action.getResult().getObject("status");
+                        com.sendTo(action.getData().getString("replyTo"),webServiceReply);
+                        newAction = createNewAction(action);
+                        newAction.setLabel(NQEConstants.DEPLOY_REMOTE_OPERATOR);
+                        newAction.setDestination((StringConstants.NODEEXECUTORQUEUE));
+                        newAction.setData(action.getResult().getObject("result"));
+                        com.sendTo(newAction.getDestination(), newAction.asJsonObject());
+
+                    }
+                    else if (label.equals(IManagerConstants.COMPLETED_MAPREDUCE)) {
+                        JsonObject webServiceReply = action.getResult().getObject("status");
+                        com.sendTo(action.getData().getString("replyTo"), webServiceReply);
+                        newAction = createNewAction(action);
+                        newAction.setLabel(NQEConstants.OPERATOR_COMPLETE);
+                        newAction.setDestination(action.getResult().getString("replyGroup"));
+                        newAction.setData(action.getResult().getObject("result"));
+                        newAction.setStatus(ActionStatus.COMPLETED.toString());
+                        com.sendTo(newAction.getDestination(), newAction.asJsonObject());
+                    }
+                    else {
+                        log.error(
+                            "Unknown COMPLETED OR INPROCESS Action received " + action.toString());
+                        return;
+                    }
+                    finalizeAction(action);
+            }
+        }
+    }
+
+    private void finalizeAction(Action action) {
+        //TODO
+        //1 inform monitor about completion (if it is completed in this logic each action requires 1 step processing
+        //2 remove from processing if necessary
+        //3 update action to persistence service
+    }
 
 
-  private void logAction(Action action) {
-    //TODO
-    //1 inform monitor about action.
-    //2 add action to processing set
-    //3 update action to persistence service
-  }
+    private void logAction(Action action) {
+        //TODO
+        //1 inform monitor about action.
+        //2 add action to processing set
+        //3 update action to persistence service
+    }
 
-  private Action createNewAction(Action action) {
-    Action result = new Action();
-    result.setId(UUID.randomUUID().toString());
-    result.setTriggered(action.getId());
-    result.setComponentType(componentType);
-    result.setStatus(PENDING.toString());
-    result.setTriggers(new JsonArray());
-    result.setOwnerId(this.id);
-    result.setProcessedBy("");
-    result.setDestination("");
-    result.setData(new JsonObject());
-    result.setResult(new JsonObject());
-    result.setLabel("");
-    result.setCategory("");
-    return result;
-  }
+    private Action createNewAction(Action action) {
+        Action result = new Action();
+        result.setId(UUID.randomUUID().toString());
+        result.setTriggered(action.getId());
+        result.setComponentType(componentType);
+        result.setStatus(PENDING.toString());
+        result.setTriggers(new JsonArray());
+        result.setOwnerId(this.id);
+        result.setProcessedBy("");
+        result.setDestination("");
+        result.setData(new JsonObject());
+        result.setResult(new JsonObject());
+        result.setLabel("");
+        result.setCategory("");
+        return result;
+    }
 
 }
