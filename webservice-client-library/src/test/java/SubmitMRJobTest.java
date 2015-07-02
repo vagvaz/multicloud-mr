794c64bb1fea140e3790bf1e38944f74c3666540
Apostolos NydriotisS <anydriotis@gmail.com>
2015-07-02 11:51:17 +0300

    Batching while putting data in the cache (client)

--- webservice-client-library/src/test/java/SubmitMRJobTest.java
+++ webservice-client-library/src/test/java/SubmitMRJobTest.java
@@ -25,6 +25,8 @@ public class SubmitMRJobTest {
   private static int port;
   private static final String DRESDEN2_IP = "80.156.73.116";
   private static final String DD1A_IP = "80.156.222.4";
+  private static final String HAMM5_IP = "5.147.254.161";
+  private static final String HAMM6_IP = "5.147.254.199";
   private static final String CACHE_NAME = "clustered";
 
   public static void main(String[] args) {
@@ -52,7 +54,6 @@ public class SubmitMRJobTest {
     jsonObject.putObject("operator", new JsonObject());
     jsonObject.getObject("operator").putObject("configuration", new JsonObject());
     jsonObject.getObject("operator").putString("name", "wordCount");
-//    jsonObject.getObject("operator").putArray("inputs", new JsonArray().add("clustered"));
     jsonObject.getObject("operator").putArray("inputs", new JsonArray().add(CACHE_NAME));
     jsonObject.getObject("operator").putString("output", "testOutputCache");
     jsonObject.getObject("operator").putArray("inputMicroClouds",
@@ -79,14 +80,24 @@ public class SubmitMRJobTest {
                    new JsonObject()
                        .putArray("dresden2", new JsonArray().add(DRESDEN2_IP))
                        .putArray("dd1a", new JsonArray().add(DD1A_IP))
+                       .putArray("hamm5", new JsonArray().add(HAMM5_IP))
+                       .putArray("hamm6", new JsonArray().add(HAMM6_IP))
         )
         .putString("reduceLocal", "true")
         .putObject("targetEndpoints",
-            new JsonObject().putArray("dresden2", new JsonArray().add(DRESDEN2_IP))
-                .putArray("dd1a", new JsonArray().add(DD1A_IP)));
+                   new JsonObject()
+                       .putArray("dresden2", new JsonArray().add(DRESDEN2_IP))
+                       .putArray("dd1a", new JsonArray().add(DD1A_IP))
+                       .putArray("hamm5", new JsonArray().add(HAMM5_IP))
+                       .putArray("hamm6", new JsonArray().add(HAMM6_IP))
+        );
     try {
 
-      String ensembleString = DD1A_IP + ":11222" + "|" + DRESDEN2_IP + ":11222";
+      String ensembleString = DD1A_IP + ":11222" + "|"
+                              + DRESDEN2_IP + ":11222" + "|"
+                              + HAMM5_IP + ":11222" + "|"
+                              + HAMM6_IP + ":11222";
+
       EnsembleCacheManager ensembleCacheManager = new EnsembleCacheManager((ensembleString));
 
       EnsembleCache ensembleCache =
@@ -102,23 +113,25 @@ public class SubmitMRJobTest {
                         "na fame pilafaki"};
 
       JsonObject data;
+      System.out.print("Loading data to '" + CACHE_NAME + "' cache\n ");
+      data = new JsonObject();
       for (int i = 0; i < 20000; i++) {
-        data = new JsonObject();
         data.putString(String.valueOf(i), lines[i % lines.length]);
-        ensembleCache.put(String.valueOf(i), new Tuple(data.toString()));
-        System.out.print(".");
+        if ((i + 1) % 100 == 0) {
+          ensembleCache.put(String.valueOf(i), new Tuple(data.toString()));
+          data = new JsonObject();
+        }
+        printProgress(i + 1);
       }
-
-//      PrintUtilities.printMap(ensembleCache);
 
 //      WebServiceClient.putObject("clustered", "id", data);  // Add data to the input cache
 
-      System.out.println("jsonObject = " + jsonObject.toString());
+      System.out.println("\njsonObject = " + jsonObject.toString());
 
       QueryStatus res = WebServiceClient.executeMapReduceJob(jsonObject, host + ":" + port);
       String id = res.getId();
       System.out.println("Submitted job. id: " + id);
-      System.out.print("Executing");
+      System.out.println("Executing...");
 
       int secs = 0;
 
@@ -130,22 +143,14 @@ public class SubmitMRJobTest {
           System.out.println(status.getErrorMessage());
           break;
         } else {
-          secs++;
-          System.out.print(".");
+          System.out.print("\r" + secs++);
           Thread.sleep(1000);
         }
       }
-      System.out.println("\n\ndd1a");
-      RemoteCacheManager remoteCacheManager = createRemoteCacheManager(DD1A_IP);
-      RemoteCache results = remoteCacheManager.getCache(id);
-      PrintUtilities.printMap(results);
 
-      System.out.println("dresden");
-      remoteCacheManager = createRemoteCacheManager(DRESDEN2_IP);
-      results = remoteCacheManager.getCache(id);
-      PrintUtilities.printMap(results);
+      printResults(id);
 
-      System.out.println("\nDONE IN: " + secs + "sec");
+      System.out.println("\nDONE IN: " + secs + " sec");
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -156,6 +161,40 @@ public class SubmitMRJobTest {
     ConfigurationBuilder builder = new ConfigurationBuilder();
     builder.addServer().host(host).port(11222);
     return new RemoteCacheManager(builder.build());
+  }
+
+  private static void printProgress(int i) {
+    if (i % 6 == 0 || i % 6 == 3) {
+      System.out.print("\r" + i + " |");
+    } else if (i % 6 == 1) {
+      System.out.print("\r" + i + " \\");
+    } else if (i % 6 == 2 || i % 6 == 5) {
+      System.out.print("\r" + i + " \u2014");
+    } else if (i % 6 == 4) {
+      System.out.print("\r" + i + " /");
+    }
+  }
+
+  private static void printResults(String id) {
+    System.out.println("\n\ndd1a");
+    RemoteCacheManager remoteCacheManager = createRemoteCacheManager(DD1A_IP);
+    RemoteCache results = remoteCacheManager.getCache(id);
+    PrintUtilities.printMap(results);
+
+    System.out.println("dresden");
+    remoteCacheManager = createRemoteCacheManager(DRESDEN2_IP);
+    results = remoteCacheManager.getCache(id);
+    PrintUtilities.printMap(results);
+
+    System.out.println("hamm5");
+    remoteCacheManager = createRemoteCacheManager(HAMM5_IP);
+    results = remoteCacheManager.getCache(id);
+    PrintUtilities.printMap(results);
+
+    System.out.println("hamm6");
+    remoteCacheManager = createRemoteCacheManager(HAMM6_IP);
+    results = remoteCacheManager.getCache(id);
+    PrintUtilities.printMap(results);
   }
 
 }
